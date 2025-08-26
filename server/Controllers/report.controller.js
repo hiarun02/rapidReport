c2d50c757f7aeaff59532b78038361834b0a0e2d
hiarun01 <hiarun02@gmail.com>
2025-08-27 00:10:36 +0530

    build submit report api

--- server/Controllers/report.controller.js
+++ server/Controllers/report.controller.js
@@ -1,3 +1,51 @@
+import {Report} from "../models/report.model.js";
+
 export const submitReport = async (req, res) => {
-  res.send("hi ");
+  try {
+    const {
+      reportId,
+      reportType,
+      imageFile,
+      incidentType,
+      title,
+      description,
+      location,
+    } = req.body;
+
+    if (
+      !reportId ||
+      !reportType ||
+      !incidentType ||
+      !title ||
+      !description ||
+      !location
+    ) {
+      return res.status(400).json({
+        message: "All fields are required.",
+        success: false,
+      });
+    }
+
+    const newReport = await Report.create({
+      reportId,
+      reportType,
+      incidentType,
+      title,
+      description,
+      location,
+      image: imageFile,
+    });
+
+    return res.status(201).json({
+      reportId: newReport.reportId,
+      message: "Report submitted successfully ✅",
+      success: true,
+    });
+  } catch (error) {
+    console.error("Error submitting report:", error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+      success: false,
+    });
+  }
 };
--- server/server.js
+++ server/server.js
@@ -2,8 +2,29 @@ import express from "express";
 import "dotenv/config";
 import {DbConnect} from "./utils/DBConnection.js";
 import submitReport from "./routes/report.route.js";
+import cors from "cors";
 
 const app = express();
+
+const mode = process.env.MODE;
+
+// CORS configuration
+if (mode === "dev") {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+    })
+  );
+} else if (mode === "prod") {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+    })
+  );
+}
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 const PORT = process.env.PORT;
